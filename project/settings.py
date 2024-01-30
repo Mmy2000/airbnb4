e7e50cacf9aa0e497e3337d5a148c4791fa0aad6
Mmy2000 <yahianada005@gmail.com>
2024-01-30 21:24:50 +0200

    fixing some issues

--- project/settings.py
+++ project/settings.py
@@ -175,7 +175,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'my552915@gmail.com'
-EMAIL_HOST_PASSWORD = '*******'
+EMAIL_HOST_PASSWORD = '******'
 
 
 
